f1ea142df37e3fc91857364beb620190d9258797
b-long <b-long@users.noreply.github.com>
2024-11-17 15:36:26 -0500

    Dask implementation that completes in 30 seconds

---
+++ moose_dj/news/tests/test_views_dask.py
@@ -0,0 +1,39 @@
+from django.test import Client
+from django.urls import reverse
+import time
+
+NUM_FAKE_TASKS = 25
+
+def test_immediate_response_with_dask():
+    client = Client()
+    response = client.post(reverse("start_task_dask"), data={"data": "foo"})
+    assert response.status_code == 200
+    assert "task_id" in response.json()
+
+    task_id = response.json()["task_id"]
+    response2 = client.get(reverse("get_task_status_dask", kwargs={"task_id": task_id}))
+    assert response2.status_code == 200
+    r2_status = response2.json()["status"]
+    assert r2_status == "running"
+
+    time.sleep(NUM_FAKE_TASKS)
+
+    attempts = 0
+    max_attempts = 8
+
+    while attempts < max_attempts:
+        time.sleep(4)
+        try:
+            response3 = client.get(reverse("get_task_status_dask", kwargs={"task_id": task_id}))
+            assert response3.status_code == 200
+
+            r3_status = response3.json()["status"]
+            r3_progress = response3.json()["progress"]
+
+            assert r3_progress >= 99
+            assert r3_status == "completed"
+            break  # Exit the loop if successful
+        except Exception:
+            attempts += 1
+            if attempts == max_attempts:
+                raise  # Raise the last exception if all attempts failed
--- moose_dj/urls.py
+++ moose_dj/urls.py
@@ -14,15 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
 # from moose_dj.news.views import start_task, get_task_status
 from moose_dj.news.views_celery import start_task, get_task_status
+from moose_dj.news.views_dask import (
+    start_task as start_task_dask,
+    get_task_status as get_task_status_dask,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("start_task", start_task, name="start_task"),
+    path("get_task_status/<str:task_id>", get_task_status, name="get_task_status"),
 
-    path('start_task', start_task, name="start_task"),
-    path('get_task_status/<str:task_id>', get_task_status, name="get_task_status")
+    path("start_task_dask", start_task_dask, name="start_task_dask"),
+    path(
+        "get_task_status_dask/<str:task_id>",
+        get_task_status_dask,
+        name="get_task_status_dask",
+    ),
 ]
